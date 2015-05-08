19660ed4f08a22c3100c836a9f46746171237457
Josue Mora <hmora@where2getit.com>
2015-05-08 15:13:28 -0700

    Updated file

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,28 +4,6 @@
     //Initializing the configuration object
       grunt.initConfig({
 
-        // Task configuration
-        concat: {
-          options: {
-            separator: ';',
-          },
-          js_frontend: {
-            src: [
-              './bower_components/jquery/jquery.js',
-              './bower_components/bootstrap/dist/js/bootstrap.js',
-              './app/assets/javascript/frontend.js'
-            ],
-            dest: './public/assets/javascript/frontend.js',
-          },
-          js_backend: {
-            src: [
-              './bower_components/jquery/jquery.js',
-              './bower_components/bootstrap/dist/js/bootstrap.js',
-              './app/assets/javascript/backend.js'
-            ],
-            dest: './public/assets/javascript/backend.js',
-          },
-        },
         less: {
           development: {
               options: {
@@ -36,22 +14,8 @@
                 "./public/assets/stylesheets/style.css":"./static/css/base.less"
               }
             }
-        },
-        uglify{
-          //...
-        },
-        phpunit{
-          //...
-        },
-        watch{
-          //...
-        },
-        // fontAwesomeVars: {
-        //   main: {
-        //     variablesLessPath: 'variables.less',
-        //     fontPath: '../bower_components/font-awesome/fonts'      //NOTE: this must be relative to FINAL, compiled .css file - NOT the variables.less file! For example, this would be the correct path if the compiled css file is main.css which is in 'src/build' and the font awesome font is in 'src/bower_components/font-awesome/fonts' - since to get from main.css to the fonts directory, you first go back a directory then go into bower_components > font-awesome > fonts. 
-        //   }
-        // }
+        }
+        
       });
 
     // Plugin loading
