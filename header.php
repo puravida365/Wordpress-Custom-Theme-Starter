5af1d352ee125f7909254ee11c729fd00def5f7b
Josue Mora <hmora@where2getit.com>
2015-05-08 14:27:15 -0700

    Changed paths to css and js files

--- header.php
+++ header.php
@@ -11,9 +11,9 @@
 <head>
 	<meta name="viewport" content="width=device-width">
 	<title>Mitholog</title>
-	<link href="<?php bloginfo('template_url'); ?>/css/style.css" rel="stylesheet" type="text/css" media="screen">
-	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery-1.11.1.min.js"></script>
-	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
+	<link href="<?php bloginfo('template_url'); ?>static/css/style.css" rel="stylesheet" type="text/css" media="screen">
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>static/js/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>static/js/main.js"></script>
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
