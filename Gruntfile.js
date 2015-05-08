d0a326cacd5d24218543ffb1ede61402d8a37837
Josue Mora <hmora@where2getit.com>
2015-05-08 15:12:11 -0700

    Enabled less compile to css via grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,9 +17,25 @@
             ],
             dest: './public/assets/javascript/frontend.js',
           },
+          js_backend: {
+            src: [
+              './bower_components/jquery/jquery.js',
+              './bower_components/bootstrap/dist/js/bootstrap.js',
+              './app/assets/javascript/backend.js'
+            ],
+            dest: './public/assets/javascript/backend.js',
+          },
         },
-        less{
-          //...
+        less: {
+          development: {
+              options: {
+                compress: true,  //minifying the result
+              },
+              files: {
+                //compiling base.less into style.css
+                "./public/assets/stylesheets/style.css":"./static/css/base.less"
+              }
+            }
         },
         uglify{
           //...
